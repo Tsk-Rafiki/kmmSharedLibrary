24d8c0464f4359bf10df5d9c0bbf1ee7b4b670e5
Sdn-w <sdn-w@yandex.ru>
2023-05-23 14:53:35 +0300

    initial commit kmm shared library

---
+++ shared_library/src/commonMain/kotlin/ru/sdn/kmmsharedlibrary/Greeting.kt
@@ -0,0 +1,9 @@
+package ru.sdn.kmmsharedlibrary
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
